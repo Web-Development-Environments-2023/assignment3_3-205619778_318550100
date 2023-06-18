84961e87f2872814210d23bb9cd4af34c8fb9b14
BIMAOR6 <73882463+BIMAOR6@users.noreply.github.com>
2023-06-18 15:51:03 +0300

    my and favorite

---
+++ src/pages/MyRecipesPage.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="container">
+      <RecipePreviewList ref="myrecipes" title="My Recipes" route_name="/users/myRecipes" class="MyRecipes"/>
+    </div>
+  
+  </template>
+  
+  <script>
+  import RecipePreviewList from '../components/RecipePreviewList.vue'
+  export default {
+      components: {
+          RecipePreviewList,
+      }
+  }
+  </script>
+  
+  <style>
+  
+  </style>
